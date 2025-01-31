16b667587f2baad6eee9d5fe5d8f434133e8de91
Erick Pinheiro <e.stefene.pinheiro@gmail.com>
2025-01-30 21:42:44 -0300

    resposta automatica aplicada, falta integrar a IA

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from services.waha import Waha
+
+app = Flask(__name__)
+
+@app.route('/chatbot/webhook/', methods=['POST'])
+
+def webhook():
+    data = request.json
+    print(f'EVENTO RECEBIDO: {data}')
+
+    waha = Waha()
+    chat_id = data['payload']['from']
+    waha.send_message(chat_id=chat_id, message='Resposta Automática =)')
+
+    if data is None:
+        return jsonify({'error': 'Invalid JSON'}), 400
+    
+    return jsonify({ 'status': 'success' }), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
